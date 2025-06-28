c7854cdbcecf626c4f17aa21d21a2e9444d95055
Ayush0015090 <kumarrayush001@gmail.com>
2025-06-28 15:54:05 +0530

    Update musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,6 +1,6 @@
 music = {
     "stealth" : "https://youtu.be/U47Tr9BB_wE?si=KnXYlQgkKdlrKDp6",
     "skyfall" : "https://youtu.be/DeumyOzKqgI?si=G0TJxnj1kQ35WWC2",
-    "gangster paradice": "https://youtu.be/fPO76Jlnz6c?si=Ql3CDZP_WZL8mI8k",
+    "gangster paradise": "https://youtu.be/fPO76Jlnz6c?si=Ql3CDZP_WZL8mI8k",
     "voices": "https://youtu.be/pM76LtznNrc?si=2aC9hPqxBVLTpb_u"
 }
